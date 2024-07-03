55f9f0ed256494ab368df355172a331e9026125d
Yasser Salama <yassersalama@Yassers-iMac.local>
2024-07-03 14:18:06 +0300

    add new book, remove existing book

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ function addBook(title, author, numberOfPages) {
     displayBooks(); // Update the display after adding a book
 }
 
+function removeBook(index) {
+    books.splice(index, 1);
+    displayBooks();
+}
+
 // Function to display books
 function displayBooks() {
     console.table(books);
@@ -19,15 +24,24 @@ function displayBooks() {
     const booksList = document.getElementById('book-list');
     booksList.innerHTML = ''; // Clear previous list
 
-    books.forEach(book => {
+    books.forEach((book,index) => {
         const bookItem = document.createElement('div');
         bookItem.className = 'book-item';
         bookItem.innerHTML = `
             <h3>${book.title}</h3>
             <p>${book.author}</p>
             <small>Pages: ${book.numberOfPages}</small>
+            <button class="remove-book-btn" data-index="${index}">Remove</button>
         `;
         booksList.appendChild(bookItem);
+    });
+
+    document.querySelectorAll('.remove-book-btn').forEach(button => {
+        button.addEventListener('click',(event) => {
+            const index = event.target.getAttribute('data-index');
+            removeBook(index);
+        });
+    
     });
 }
 
@@ -35,7 +49,29 @@ function displayBooks() {
 function handleFormSubmit(event){
     event.preventDefault(); // prevent the form from submitting 
 
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = parseInt(document.getElementById('pages').value,10);
+
+    addBook(title,author,pages);
+
+    document.getElementById('new-book-form').reset();
+
+    document.getElementById('book-dialog').close();
 }
+
+// Listen to add new book button click
+document.getElementById('add-book-btn').addEventListener('click',() => {
+    document.getElementById('book-dialog').showModal();
+});
+
+// Listen to close add book dialog
+document.getElementById('close-dialog-btn').addEventListener('click',()=>{
+    document.getElementById('book-dialog').close();
+});
+
+// Listen to submit new book click
+document.getElementById('new-book-form').addEventListener('submit', handleFormSubmit);
 
 // Add some test data
 addBook('The Great Gatsby', 'F. Scott Fitzgerald', 180);
